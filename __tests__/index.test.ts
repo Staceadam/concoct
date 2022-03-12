e16414ed143ac6d25609f26ee33835cf3293868a
Stacy Adam <staceadam@gmail.com>
2022-03-11 18:55:49 -0600

    boilerplate and tests

---
+++ __tests__/index.test.ts
@@ -0,0 +1,5 @@
+test('grabs output from config file', () => {})
+test('generates types based on config file', () => {})
+test('outputs types into consumer of packages node modules like prisma', () => {})
+test('initializes a Concoct singlton singleton', () => {})
+test('has methods exposed on singleton based on config structure', () => {})
